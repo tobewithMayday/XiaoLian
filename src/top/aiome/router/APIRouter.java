a890376ce1a66c7246b441e687f0856337c72792
Aiome <420454139@qq.com>
2016-11-21 19:56:45 +0800

    上传项目

---
+++ src/top/aiome/router/APIRouter.java
@@ -0,0 +1,19 @@
+package top.aiome.router;
+
+import com.jfinal.config.Routes;
+import top.aiome.api.*;
+
+/**
+ * @author malongbo
+ */
+public class APIRouter extends Routes {
+    @Override
+    public void config() {
+        //公共api
+        add("/api", CommonAPIController.class);
+        //用户相关
+        add("/api/account", AccountAPIController.class);
+        //文件相关
+        add("/api/fs",FileAPIController.class);
+    }
+}
---
+++ src/top/aiome/temp.java
@@ -0,0 +1,18 @@
+/** 
+* 文件名称:.java
+* 姓		名:马红岩
+* 学		号:2014011791
+* 班		级:6班
+* 时		间:2016年11月19日
+* 程序说明:
+*			
+**/ 
+package top.aiome;
+
+import com.jfinal.core.Controller;
+
+public class temp extends Controller{
+	public void index(){
+		render("index.html");
+	}
+}
